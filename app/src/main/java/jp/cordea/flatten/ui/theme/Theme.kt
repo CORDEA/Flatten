a91e7932ed7b8e2d01c6d713ade65042087e46c1
Yoshihiro Tanaka <contact@cordea.jp>
2022-11-27 13:34:30 +0900

    change the target sdk version

--- app/src/main/java/jp/cordea/flatten/ui/theme/Theme.kt
+++ app/src/main/java/jp/cordea/flatten/ui/theme/Theme.kt
@@ -1,19 +1,14 @@
 package jp.cordea.flatten.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -45,7 +40,7 @@ fun FlattenTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
@@ -55,8 +50,11 @@ fun FlattenTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window.run {
+                statusBarColor = colorScheme.primary.toArgb()
+                WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars =
+                    darkTheme
+            }
         }
     }
 
