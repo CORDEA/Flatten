6cea794b6d935b5613aad5dec26e269b0b5da8d1
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-03 11:20:04 +0900

    returns a flow

--- app/src/main/java/jp/cordea/flatten/repository/UserRepository.kt
+++ app/src/main/java/jp/cordea/flatten/repository/UserRepository.kt
@@ -5,13 +5,19 @@ import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import jp.cordea.flatten.data.User
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class UserRepository(
     private val client: HttpClient
 ) {
-    suspend fun find(): User = client.get {
-        url {
-            appendPathSegments("me")
-        }
-    }.body()
+    fun find(): Flow<User> = flow {
+        emit(
+            client.get {
+                url {
+                    appendPathSegments("me")
+                }
+            }.body()
+        )
+    }
 }
