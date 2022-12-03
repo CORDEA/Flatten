16eb8c64607b0d7f8512302264f319334c08a2cf
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-03 11:25:50 +0900

    fetch scores

--- app/src/main/java/jp/cordea/flatten/ui/HomePresenter.kt
+++ app/src/main/java/jp/cordea/flatten/ui/HomePresenter.kt
@@ -1,17 +1,29 @@
 package jp.cordea.flatten.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import jp.cordea.flatten.repository.ScoreRepository
 import jp.cordea.flatten.repository.UserRepository
 
 sealed class HomeModel {
     object Loading : HomeModel()
+    class Loaded(val items: List<HomeItemModel>) : HomeModel()
 }
+
+class HomeItemModel(val title: String)
 
 @Composable
 fun homePresenter(
     userRepository: UserRepository,
     scoreRepository: ScoreRepository
 ): HomeModel {
-    return HomeModel.Loading
+    val userState by userRepository.find().collectAsState(initial = null)
+    userState?.let { user ->
+        val scores by scoreRepository.findLikes(user.id).collectAsState(initial = emptyList())
+        if (scores.isEmpty()) {
+            return HomeModel.Loading
+        }
+        return HomeModel.Loaded(scores.map { HomeItemModel(it.title) })
+    } ?: return HomeModel.Loading
 }
