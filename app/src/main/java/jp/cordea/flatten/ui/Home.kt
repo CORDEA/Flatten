bbb39b95ef2faac53b0a20b6913107fa56da178a
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-03 12:33:53 +0900

    display additional info

--- app/src/main/java/jp/cordea/flatten/ui/Home.kt
+++ app/src/main/java/jp/cordea/flatten/ui/Home.kt
@@ -1,17 +1,23 @@
 package jp.cordea.flatten.ui
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import io.ktor.http.auth.*
 import kotlinx.coroutines.flow.Flow
 import org.koin.androidx.compose.get
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -54,10 +60,52 @@ fun Home(models: Flow<HomeModel> = get()) {
 
 @Composable
 private fun HomeItem(model: HomeItemModel) {
-    Row {
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = model.title
+            )
+            Text(
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                text = model.subtitle
+            )
+        }
         Text(
-            modifier = Modifier.padding(16.dp),
-            text = model.title
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically),
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            maxLines = 1,
+            overflow = TextOverflow.Visible,
+            text = DateTimeFormatter.ofPattern("M/dd").format(model.createdAt)
         )
     }
+}
+
+@Preview
+@Composable
+fun HomeItem() {
+    HomeItem(
+        model = HomeItemModel(
+            title = "title",
+            subtitle = "subtitle",
+            createdAt = LocalDateTime.now()
+        )
+    )
 }
